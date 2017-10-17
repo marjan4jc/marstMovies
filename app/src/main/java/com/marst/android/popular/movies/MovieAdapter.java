f3069ccc43ca1f7e4ed8102d75c4684d7614c189
marst <marjan4jc@gmail.com>
2017-10-17 10:02:54 +0200

    not needed NonNull import removed!

--- app/src/main/java/com/marst/android/popular/movies/MovieAdapter.java
+++ app/src/main/java/com/marst/android/popular/movies/MovieAdapter.java
@@ -2,7 +2,6 @@ package com.marst.android.popular.movies;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
